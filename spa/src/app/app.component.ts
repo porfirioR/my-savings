ea6a888659df12e91da2c419e7cf5afd271d832b
PorfirioR <porfirioperezgon@gmail.com>
2024-01-25 23:43:29 -0300

    create-angular-project

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-savings';
+  protected product = environment.production
 }
