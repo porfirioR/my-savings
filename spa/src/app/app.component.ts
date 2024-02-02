33b1bf6ccd055c0827ce6bb4842bafe2e8324633
PorfirioR <porfirioperezgon@gmail.com>
2024-02-01 23:12:42 -0300

    create-angular-project

--- spa/src/app/app.component.ts
+++ spa/src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
 import { DynamicTableComponent } from './components/dynamic-table/dynamic-table.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    DynamicTableComponent
+    DynamicTableComponent,
+    DashboardComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- spa/src/app/components/dashboard/dashboard.component.ts
+++ spa/src/app/components/dashboard/dashboard.component.ts
@@ -8,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  toggleFloatingButtons() {
+    const floatingButtons = document.getElementById('floatingButtons');
+    if (floatingButtons) {
+      floatingButtons.style.display = (floatingButtons.style.display === 'block') ? 'none' : 'block';
 
+    }
+  }
+  
+  handleAction(option: any) {
+    alert(`Botón ${option} presionado`);
+  }
 }
