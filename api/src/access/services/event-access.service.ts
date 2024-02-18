41dc2373a435002343e7027cce5b11f5a6f96f16
PorfirioR <porfirioperezgon@gmail.com>
2024-02-18 01:45:32 -0300

    7-add-nestjs

--- api/src/access/services/event-access.service.ts
+++ api/src/access/services/event-access.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { DbContextService } from './db-context.service';
 import { SupabaseClient } from '@supabase/supabase-js';
+import { DbContextService } from './db-context.service';
 import { TableEnum } from '../contract/table.enum';
-import { Events } from '../contract/entities/event.entity';
+import { CreateEventAccessRequest } from '../contract/events/create-event-access-request';
+import { EventAccessModel } from '../contract/events/event-access-model';
 
 @Injectable()
 export class EventAccessService {
@@ -12,11 +13,30 @@ export class EventAccessService {
     this.eventContext = this.dbContextService.getConnection();
   }
 
-  public getPublicEvents = async (): Promise<Events[]> => {
+  public getPublicEvents = async (): Promise<EventAccessModel[]> => {
     const { data, error } = await this.eventContext
       .from(TableEnum.Events)
-      .select('*');
+      .select('*')
+      .eq('IsPublic', true);
     if (error) throw new Error(error.message);
-    return data?.map((x) => ({ ...x, _className: Events }));
+    return data?.map((x) => ({ ...x, _className: EventAccessModel }));
+  };
+
+  public getMyEvents = async (id: string): Promise<EventAccessModel[]> => {
+    const { data, error } = await this.eventContext
+      .from(TableEnum.Events)
+      .select('*')
+      .eq('AuthorId', id);
+    if (error) throw new Error(error.message);
+    return data?.map((x) => ({ ...x, _className: EventAccessModel }));
+  };
+
+
+  public createEvent = async (accessRequest: CreateEventAccessRequest): Promise<boolean> => {
+    const event  = await this.eventContext
+      .from(TableEnum.Events)
+      .upsert([accessRequest]);
+    if (event.error) throw new Error(event.error.message);
+    return true;
   };
 }
