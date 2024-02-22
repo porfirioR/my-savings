14911046ae28959a11cdf48975c6a86b8076b56c
PorfirioR <porfirioperezgon@gmail.com>
2024-02-21 23:56:56 -0300

    7-add-nestjs

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,17 +1,10 @@
-import { APP_FILTER } from '@nestjs/core';
-import { AllExceptionsFilter } from './controller/services/exception.filter';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { ControllerModule } from './controller/controller.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [ConfigModule.forRoot(), ControllerModule],
   controllers: [],
-  providers: [
-    {
-      provide: APP_FILTER,
-      useClass: AllExceptionsFilter
-    }
-  ]
+  providers: []
 })
 export class AppModule {}
--- api/src/controller/controller.module.ts
+++ api/src/controller/controller.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { UsersController } from './services/users.controller';
 import { EventsController } from './services/events.controller';
+import { AllExceptionsFilter } from './services/exception.filter';
 import { ManagerModule } from '../manager/manager.module';
 
 @Module({
@@ -9,6 +11,11 @@ import { ManagerModule } from '../manager/manager.module';
     UsersController,
     EventsController
   ],
-  providers: [],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter
+    }
+  ],
 })
 export class ControllerModule {}
