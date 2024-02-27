cad3da625efc94d869140893ddc51eb4cb3d5164
PorfirioR <porfirioperezgon@gmail.com>
2024-02-27 00:10:04 -0300

    2-add-guard

--- spa/src/app/models/api/update-event-api-request.ts
+++ spa/src/app/models/api/update-event-api-request.ts
@@ -1,5 +1,20 @@
-import { CreateEventApiRequest } from "./create-event-api-request";
+import { CreateEventApiRequest } from ".";
 
 export class UpdateEventApiRequest extends CreateEventApiRequest {
-
+  constructor(
+    public id: number,
+    name: string,
+    authorId: number,
+    description: string,
+    date: Date,
+    isPublic: boolean
+  ) {
+    super(
+      name,
+      authorId,
+      description,
+      date,
+      isPublic
+    );
+  }
 }
