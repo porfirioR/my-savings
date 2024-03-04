30045f5c0173548da4c07278aa8b1e1b9b4a7910
PorfirioR <porfirioperezgon@gmail.com>
2024-03-04 02:07:11 -0300

    11-show-public-events

--- spa/src/app/app.routes.ts
+++ spa/src/app/app.routes.ts
@@ -4,18 +4,19 @@ import { MyEventsComponent } from './components/my-events/my-events.component'
 
 export const routes: Routes = [
   {
-    path: 'principal',
-    title: 'Principal',
-    loadComponent: () => HomeComponent
-  },
-  {
-    path: 'my-events',
-    title: 'My events',
-    loadComponent: () => MyEventsComponent
-  },
-  {
     path: '',
-    redirectTo: 'principal',
-    pathMatch: 'full'
+    title: 'Principal',
+    children: [
+      {
+        path: '',
+        title: 'Principal',
+        loadComponent: () => HomeComponent
+      },
+      {
+        path: 'my-events',
+        title: 'My events',
+        loadComponent: () => MyEventsComponent
+      },
+    ]
   }
 ]
