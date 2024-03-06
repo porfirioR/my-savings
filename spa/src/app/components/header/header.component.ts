03c947f7e827a49e32f56ca573d01a2e1393c4ec
PorfirioR <porfirioperezgon@gmail.com>
2024-03-06 18:49:00 -0300

    add profile component in header

--- spa/src/app/components/header/header.component.ts
+++ spa/src/app/components/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProfileComponent } from "../profile/profile.component";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
-  standalone: true,
-  imports: [
-    RouterModule
-  ]
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrl: './header.component.css',
+    standalone: true,
+    imports: [
+      RouterModule,
+      ProfileComponent
+    ]
 })
 export class HeaderComponent {
+  protected openProfile = (): void => {
+    throw new Error('Method not implemented.');
+  }
+
 
 }
