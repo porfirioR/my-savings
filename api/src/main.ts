fda8df2015eb1ee87e9646e37c17333c1ef0644a
PorfirioR <porfirioperezgon@gmail.com>
2024-03-26 18:37:25 -0400

    update

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { config } from 'dotenv';
-import { PrivateEndpointGuard } from './controller/guards/private-endpoint.guard';
+import { PrivateEndpointGuard } from './host/guards/private-endpoint.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
