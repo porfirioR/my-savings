eeeeeab17b6cb67ca8c431421e3cd262e70e8ddd
PorfirioR <porfirioperezgon@gmail.com>
2024-11-28 18:45:28 -0300

    53-update-der

--- api/src/access/contract/payments/payment-access-model.ts
+++ api/src/access/contract/payments/payment-access-model.ts
@@ -1,2 +1,9 @@
 export class PaymentAccessModel {
+  constructor(
+    public id: number,
+    public amount: number,
+    public date: Date,
+    public refund: boolean,
+    public savingId: number
+  ) {}
 }
---
+++ api/src/access/contract/type/type-access-model.ts
@@ -0,0 +1,6 @@
+export class TypeAccessModel {
+  constructor(
+    public id: number,
+    public name: string
+  ) {}
+}
