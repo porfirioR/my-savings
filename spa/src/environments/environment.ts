a20fb597a7b796d3f0c6b0d515b4275194791dff
PorfirioR <porfirioperezgon@gmail.com>
2024-11-29 14:47:50 -0300

    53-update-der

--- spa/src/environments/environment.ts
+++ spa/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   baseUrl: 'api/',
   production: true,
   webPush: {
-    publicKey: 'BJdCj-cZmqQz3fahRTCDkbVcOSKipplKwWU8I_eOfAh7I-dWtp6WLyxaD1vchW4ztAQFZr54BwOKicbn16236J0',
-    privateKey:'mTkquneMpVdYJtmJec1SAIfEX4MGBVhsIEoGA0Kd7H8'
+    publicKey: '',
+    privateKey:''
   }
 }
