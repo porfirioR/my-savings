6f680d72d6882d1568f6642aa9d5e87930727767
PorfirioR <porfirioperezgon@gmail.com>
2024-12-27 14:54:28 -0300

    57-spa-add-saving

---
+++ spa/src/app/components/upsert-saving/upsert-saving.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-upsert-saving',
+  templateUrl: './upsert-saving.component.html',
+  styleUrls: ['./upsert-saving.component.css']
+})
+export class UpsertSavingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
