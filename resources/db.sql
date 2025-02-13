fd74c425ee8be6e0233d14ebf2c8bedd0297cbe9
PorfirioR <porfirioperezgon@gmail.com>
2025-02-13 15:12:18 -0300

    57-spa-add-saving

--- resources/db.sql
+++ resources/db.sql
@@ -1,4 +1,4 @@
-INSERT INTO Currency (Id, Name, Symbol, Country) VALUES 
+INSERT INTO currencies (Id, Name, Symbol, Country) VALUES 
 (1, 'United States Dollar', '$', 'United States'),
 (2, 'Euro', '€', 'Eurozone'),
 (3, 'British Pound Sterling', '£', 'United Kingdom'),
@@ -20,13 +20,13 @@ INSERT INTO Currency (Id, Name, Symbol, Country) VALUES
 (19, 'Surinamese Dollar', '$', 'Suriname'),
 (20, 'Uruguayan Peso', '$', 'Uruguay');
 
-INSERT INTO Periods (Id, Name, Symbol, Quantity, userId?) VALUES 
+INSERT INTO Periods (Id, Name, quantity) VALUES 
 (1, 'Day', 365),
 (2, 'Week', 52),
 (3, 'Month', 12),
 (4, 'Custom', -1);
 
-INSERT INTO types (Id, Description) VALUES 
-(1, 'Progresive', 'Amounts increase by adding a constant value'),
+INSERT INTO types (Id, name, Description) VALUES 
+(1, 'Progressive', 'Amounts increase by adding a constant value'),
 (2, 'Fixed amount', 'Fixed amount for the duration of the loan'),
 (3, 'Total', 'Total amount of the loan');
